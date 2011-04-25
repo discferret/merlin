e49ba335f4802c2adc012d5293dec7835da49749
Philip Pemberton <philpem@philpem.me.uk>
2011-04-25 15:10:52 +0100

    add random quips

--- src/winMain.cpp
+++ src/winMain.cpp
@@ -85,6 +85,58 @@ bool winMain::Create( wxWindow* parent, wxWindowID id, const wxString& caption, 
 	SetIcon(GetIconResource(wxT("discferret-notext32.png")));
 	Centre();
 ////@end winMain creation
+
+#ifndef DISABLE_QUIPS
+	// Random quips... inspired by ImgBurn.
+	wxArrayString quips;
+	quips.Add(_T("Magic Quip System powered by dumb luck and brute force!"));
+	quips.Add(_T("Use the Force, Luke!"));														// Star Wars
+	quips.Add(_T("Use the Source, Luke!"));													// because after all, this app is OSS/FS...
+	quips.Add(_T("Runaway train, never going back... wrong way on a one-way track..."));		// Soul Asylum / Runaway Train
+	quips.Add(_T("Perfectionism is the enemy of 'just good enough'."));						// Mark Jackson, Echostar CTO (hi, Mark!)
+	quips.Add(_T("I have altered the deal. Pray I do not alter it further."));					// More Star Wars
+	quips.Add(_T("Dead or alive, you're coming with me."));									// Robocop
+	quips.Add(_T("Baldrick, I have a cunning plan."));											// Blackadder
+	quips.Add(_T("DiscFerret: it's not just a cute name."));
+	quips.Add(_T("Coming soon: in-status-bar advertising!"));									// God I hope not.
+	quips.Add(_T("There are no stupid answers, Cartman, only stupid people."));				// South Park.
+	quips.Add(_T("You were only supposed to blow the bloody doors off!"));						// The Italian Job
+	quips.Add(_T("When in trouble, when in doubt, run in circles, scream and shout."));		// Freefall #47: http://freefall.purrsia.com/ff100/fv00047.htm
+	quips.Add(_T("All technology, no matter how primitive, is magic to those who don't understand it."));	// Freefall #255: http://freefall.purrsia.com/ff300/fv00255.htm
+	quips.Add(_T("I do not read organic expressions well and yours still worry me."));			// Freefall #2021: http://freefall.purrsia.com/ff2100/fv02021.htm
+	// I should probably stop quoting Freefall before Mark Stanley sics a lawyer on me.
+	quips.Add(_T("YOW! Are we having fun yet?"));												// Zippy the Pinhead
+	quips.Add(_T("Java is an affront to software engineering."));								// Seriously. I spent TWO DAYS trying to get this app working in Java.
+																								// After switching to wxWidgets I had a working prototype in less than a day.
+	quips.Add(_T("What have the Romans ever done for us?"));									// Monty Python's Life of Brian
+	quips.Add(_T("Do not fold, bend, spindle or mutilate."));									// IBM punched cards (allegedly)
+	quips.Add(_T("Smoke me a kipper, I'll be back for breakfast."));							// 'Ace' Rimmer (Red Dwarf)
+	quips.Add(_T("It is a miracle that curiosity survives formal education."));				// Albert Einstein
+	quips.Add(_T("Beware of computer programmers that carry screwdrivers."));
+	quips.Add(_T("And thus the Lord spake: 'Verily, ye shalt bewareth of the Programmer that carryeth a Soldering Iron.'"));
+	quips.Add(_T("You can be my Yoko Ono, you can follow me wherever I go..."));				// Barenaked Ladies -- Be My Yoko Ono
+	quips.Add(_T("It's all been done before (whoo-hoo-hoo!). It's all been done... before!"));	// Barenaked Ladies -- It's All Been Done
+	quips.Add(_T("Your mother was a hamster and your father smelt of elderberries."));			// Monty Python and the Holy Grail
+	quips.Add(_T("Made it, Ma! Top of the world!"));											// James Cagney in White Heat
+	quips.Add(_T("I'm going to make him an offer he can't refuse."));							// The Godfather
+	quips.Add(_T("Toto, I've got a feeling we're not in Kansas anymore."));					// The Wizard of Oz
+	quips.Add(_T("What we've got here is a failure to communicate."));							// Cool Hand Luke
+	quips.Add(_T("Rosebud."));																	// Citizen Kane
+	quips.Add(_T("I'm as mad as hell, and I'm not going to take this anymore!"));				// Network
+	quips.Add(_T("Gentlemen. You can't fight in here. This is the War Room!"));				// Dr. Strangelove
+	quips.Add(_T("Gratuitous acts of senseless violence are my forte!"));						// Sam and Max Hit The Road
+	quips.Add(_T("I don't think this is a natural occurrence, Max. In fact, I think we're witnessing a celestial convergence of some sort.")); // Sam & Max
+	quips.Add(_T("I'm sure this thing is somehow useful, but I'll be damned if I know how."));	// Sam and Max Hit The Road
+	quips.Add(_T("I'm thinking of a number between one and ten, and... I don't know why."));	// Sam and Max Hit The Road
+	quips.Add(_T("Looks to me like a margionally vulnerable hostage situation!"));				// Sam and Max Hit The Road
+
+	// Now we seed the RNG and pick a random quip
+	srand(time(NULL));
+	statusBar->SetStatusText(quips[rand() % quips.GetCount()]);
+#else
+	statusBar->SetStatusText(_("Ready."));
+#endif
+
     return true;
 }
 
@@ -181,7 +233,7 @@ void winMain::CreateControls()
 
 bool winMain::ShowToolTips()
 {
-    return true;
+	return true;
 }
 
 /*
@@ -270,7 +322,7 @@ void winMain::OnFileOpenCatweaselIMGClick( wxCommandEvent& event )
 //		MainEditBox->LoadFile(CurrentDocPath); //Opens that file
 
 		// Set the Title to reflect the file open
-		SetTitle(wxString("Merlin - ") << OpenDialog->GetFilename());
+		SetTitle(wxString(_T("Merlin - ")) << OpenDialog->GetFilename());
 	}
  
 	// Clean up after ourselves
